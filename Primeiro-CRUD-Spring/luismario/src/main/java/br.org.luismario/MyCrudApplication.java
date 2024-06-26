5cdbf93d814deee8b0ccf78db6910e603e586eca
Luis Mario <luismario337y@gmail.com>
2024-06-25 21:33:39 -0300

    first commit: create project spring

---
+++ Primeiro-CRUD-Spring/luismario/src/main/java/br.org.luismario/MyCrudApplication.java
@@ -0,0 +1,13 @@
+package br.org.luismario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCrudApplication.class, args);
+	}
+
+}
---
+++ Primeiro-CRUD-Spring/luismario/src/main/java/br.org.luismario/models/GameModel.java
@@ -0,0 +1,9 @@
+package br.org.luismario.models;
+
+@Table
+public class GameModel {
+    public String name;
+    public decimal priceGame;
+    public description;
+
+}
